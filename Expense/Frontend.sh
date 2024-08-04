2244148ff70b642784cbeb4988d7846cfc87c46d
Ravi Kumar <kshylaja423@gmail.com>
2024-08-04 17:01:08 +0530

    Auto push

--- Expense/Frontend.sh
+++ Expense/Frontend.sh
@@ -1,4 +1,5 @@
 dnf install nginx -y
+cp proxy.conf /etc/nginx/default.d/expense.conf
 systemctl enable nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
@@ -6,6 +7,6 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 pwd
 ls -ltr
-cp proxy.conf /etc/nginx/default.d/expense.conf
+
 systemctl restart nginx
 echo ""**Frontend Installation is completed**"
