f600395b4c98caa3d6f5f6cfda608d33342b1fc2
Ravi Kumar <kshylaja423@gmail.com>
2024-08-06 16:48:06 +0530

    Auto push

---
+++ Expense/Backend.sh
@@ -0,0 +1,18 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+useradd expense
+mkdir /app
+curl -o /tmp/backend.zip https://expense-web-app.s3.amazonaws.com/backend.zip 
+cp backend.service /etc/systemd/system/backend.service
+cd /app 
+unzip /tmp/backend.zip
+cd /app
+npm install
+chmod -R 775 /app
+chown -R expense:expense /app
+dnf install mysql-server -y
+mysql -h localhost -uroot -pExpenseApp@1 < /app/schema/backend.sql 
+systemctl daemon-reload
+systemctl enable backend 
+systemctl start backend
