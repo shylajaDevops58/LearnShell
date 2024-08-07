b5623e887cbcc905bd91cdc57e2253d25dddece2
Ravi Kumar <kshylaja423@gmail.com>
2024-08-07 19:13:08 +0530

    Auto push

---
+++ bash-scripting/005-specialVars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+# Special Variables are the pre-defined variables with specific special pre-defined powers.
+
+# Special Variables are $0 to $9, $* , $@, $# ,  $$
+echo $0
+echo $9
+echo $*
+echo $@
+echo $#
+echo $$
