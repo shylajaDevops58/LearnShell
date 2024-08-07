fb274e33543089dcdf056382cb054c9480e022a1
Ravi Kumar <kshylaja423@gmail.com>
2024-08-07 19:00:08 +0530

    Auto push

--- bash-scripting/004-dynamicvariables.sh
+++ bash-scripting/004-dynamicvariables.sh
@@ -3,6 +3,6 @@
 # DATE="07AUG2024"                # This is called hardCoding 
  
 DATE=$(date +%F)                  # This is how we can dynamically fetch the values
-Exec=#1
+Exec=$1
 Dept=$2
 echo "script is executed by $Exec from department $Dept"
