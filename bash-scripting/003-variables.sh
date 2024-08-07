695991142571232287ad017471c6a71fb5fa31ad
Ravi Kumar <kshylaja423@gmail.com>
2024-08-07 18:56:15 +0530

    Auto push

--- bash-scripting/003-variables.sh
+++ bash-scripting/003-variables.sh
@@ -2,10 +2,10 @@ a=10
 b=2.5
 name=shylu
 
-echo $a 
-echo ${a}
-echo "$a"
-echo {$a}
+echo $a           # output is 10 
+echo ${a}         # output is 10 
+echo "$a"         # output is 10 
+echo {$a}         # output is {10} 
 echo $b
 echo $name
 echo -e "value of a is \e[32m $a \e[0m"
